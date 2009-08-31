8ae79216899da4f75c85b211d04d5d2b442e0c89
so-miya <so-miya@f7c1ead4-3e12-498c-9bba-8862e68d3f10>
2009-08-31 14:05:50 +0000

    UnNetHack SVN版rev.284をベースにした。 r283 Don't give a wand of death to Cthulhu. Such a wand is rather useless against the player in this stage. クトゥルフに死の杖を与えないことにした。 この手の杖はこの段階までくるようなプレイヤーにはほとんど効かない。
    r284
    Cthulhu gets an amulet of reflection
    クトゥルフに反射の魔除けを与えた。

--- src/makemon.c
+++ src/makemon.c
@@ -666,7 +666,7 @@ register struct	monst	*mtmp;
 		 *      until Moloch's Sanctum is rearranged */
 		if (ptr == &mons[PM_CTHULHU]) {
 			(void)mongets(mtmp, AMULET_OF_YENDOR);
-			(void)mongets(mtmp, WAN_DEATH);
+			(void)mongets(mtmp, AMULET_OF_REFLECTION);
 			(void)mongets(mtmp, POT_FULL_HEALING);
 		}
 	    default:
