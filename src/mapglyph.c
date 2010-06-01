46d19ebeb954f9262cb56ad35966489c4357dc8f
so-miya <so-miya@f7c1ead4-3e12-498c-9bba-8862e68d3f10>
2010-06-01 14:08:30 +0000

    UnNetHack SVN版rev.502を適用した。 Fix: inverse video for stacks of items with corpses on top 修正：一番上に死体がある複数のアイテムを反転表示

--- src/mapglyph.c
+++ src/mapglyph.c
@@ -170,6 +170,11 @@ unsigned *ospecial;
 #endif
 	    mon_color(offset);
 	    special |= MG_CORPSE;
+	    /* use inverse video for multiple items */
+	    if (level.objects[x][y] &&
+	        level.objects[x][y]->nexthere) {
+		special |= MG_INVERSE;
+	    }
     } else if ((offset = (glyph - GLYPH_DETECT_OFF)) >= 0) {	/* mon detect */
 	ch = monsyms[(int)mons[offset].mlet];
 #ifdef ROGUE_COLOR
