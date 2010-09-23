0e52efa686b0d039e4cf4f552e5dc37a528cccf8
so-miya <so-miya@f7c1ead4-3e12-498c-9bba-8862e68d3f10>
2010-09-23 02:29:26 +0000

    UnNetHack SVN版rev.674を適用した。 update version number to 3.5.4 3.5.4にバージョン番号を更新

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -9,7 +9,7 @@
 /*
  * PATCHLEVEL is updated for each release.
  */
-#define PATCHLEVEL	3
+#define PATCHLEVEL	4
 /*
  * Incrementing EDITLEVEL can be used to force invalidation of old bones
  * and save files.
@@ -48,7 +48,7 @@
  * PP = patch level, ee = edit level, L = literal suffix "L",
  * with all four numbers specified as two hexadecimal digits.
  */
-/* #define VERSION_COMPATIBILITY 0x03050000L */	/* 3.5.0-0 */
+#define VERSION_COMPATIBILITY 0x03050000L	/* 3.5.0-0 */
 
 
 /*****************************************************************************/
